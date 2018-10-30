775309b479ec3abae7ccce9369a7bc3483e8ad45
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-30 13:17:19 -0500

    added filter to explore table

--- src/components/Tables/ExploreTable.js
+++ src/components/Tables/ExploreTable.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ProjectButton from '../LinkButtons/ProjectButton';
-import  { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import  { Table, TableHead, TableBody, TableRow, TableCell, Input, InputLabel } from '@material-ui/core';
 import PlayButton from '../LinkButtons/PlayButton';
 
 class ProjectTable extends Component {
 
+  state = {
+    filter: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      filter: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
+        <InputLabel>
+          Search by Name: {' '}
+          <Input type="text" value={this.state.filter} onChange={this.handleChange} />
+        </InputLabel>
         <Table>
           <TableHead>
             <TableRow>
@@ -20,7 +34,7 @@ class ProjectTable extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.props.table.map(project => (
+            {this.props.table.filter(project => project.name.includes(this.state.filter)).map(project => (
               <TableRow key={project.id}>
                 <TableCell>{project.name}</TableCell>
                 <TableCell>{project.artist}</TableCell>
